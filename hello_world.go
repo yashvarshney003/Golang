4c0d13ed5a88e454749aa633ee4c91297e1f13ba
yashvarshney003 <59112785+yashvarshney003@users.noreply.github.com>
2024-12-01 13:03:34 +0530

    DAY 1
    This is simplest program in go.

---
+++ hello_world.go
@@ -0,0 +1,5 @@
+package main // the package main declaration holds a special meaning. It indicates that the package is an executable program, not a reusable library. This is specific to programs that produce a binary executable.
+import "fmt" // importing package called format
+func main(){
+  fmt.Println("Yash World")
+  {
